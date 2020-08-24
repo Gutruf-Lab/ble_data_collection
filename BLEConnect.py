0f463cc0c4ad761f9b50c86ea5397ddd65880a31
kkasper <kkasper@github.com>
2020-08-23 17:52:06 -0700

    Data streaming.

--- BLEConnect.py
+++ BLEConnect.py
@@ -1,40 +1,110 @@
 from bleak import BleakClient, discover
 import asyncio
+import pandas as pd
+import os
+from os import path
+import datetime
+from datetime import timedelta
+from collections import defaultdict
+
+from bleak.exc import BleakDotNetTaskError, BleakError
+
+new_data = defaultdict(lambda: "Not found")
+
+# Time in milliseconds (!)
+storage_timing = 750
 
 
 def notification_handler(sender, data):
-    """Simple notification handler which prints the data received."""
-    print("{0}: {1}".format(handle_desc_pairs[sender], int.from_bytes(data, byteorder='little')))
+    outgoing_data = pd.DataFrame()
+    read_data = pd.DataFrame()
+    global new_data
+
+    if len(new_data.keys()) < 2:
+        new_data["Time:"] = [datetime.datetime.now()]
+
+    if handle_desc_pairs[sender] == "Temp Value:":
+        if new_data[handle_desc_pairs[sender]] == "Not found":
+            new_data[handle_desc_pairs[sender]] = [int.from_bytes(data, byteorder='little')]
+    else:
+        if new_data["Temp Value:"] != "Not found":
+            new_data[handle_desc_pairs[sender]] = [int.from_bytes(data, byteorder='little')]
+
+    # print("\nTime {0}. Dif: {1}\n".format(datetime.datetime.now(), datetime.datetime.now() - new_data["Time:"][0]))
+    if len(new_data.keys()) > 2:
+        if datetime.datetime.now() - timedelta(milliseconds=storage_timing) >= new_data["Time:"][0]:
+            new_df = pd.DataFrame(new_data)
+            print(new_df)
+
+            new_df.to_csv('data/streamed_data.csv',
+                                 index=False,
+                                 header=False,
+                                 mode='a'  # append data to csv file
+                                 )
+            new_data["Time:"] = [datetime.datetime.now()]
+            del new_data["Temp Value:"]
+            del new_data["Strain Value:"]
 
 
-async def run():
-    address = ''
-    while address == '':
-        devices = await discover(timeout=2)
-        for d in devices:
-            print(d)
-            if d.name == 'GUTRUF LAB v0.01':
-                address = d.address
-                print('Device found.')
-        print('----')
+async def run(event_loop):
+    while True:
+        address = ''
+        try:
+            while address == '':
+                devices = await discover(timeout=1)
+                for d in devices:
+                    print(d)
+                    if d.name == 'GUTRUF LAB v0.01':
+                        address = d.address
+                        print('Device found.')
+                print('----')
 
-    async with BleakClient(address, loop=loop) as client:
-        x = await client.is_connected()
-        services = await client.get_services()
-        for s in services:
-            for char in s.characteristics:
-                # print(f'Characteristic: {char}')
-                print(f'[{char.uuid}] {char.description}:, {char.handle}, {char.properties}')
-                handle_desc_pairs[char.handle] = char.description
+            async with BleakClient(address, loop=event_loop) as client:
+                x = await client.is_connected()
+                # disconnected_event = asyncio.Event()
+                # async def disconnect_callback(client, future):
+                #     print("Disconnected callback called!")
+                #     loop.call_soon_threadsafe(disconnected_event.set)
+                #     print("Connection lost. Returning to BLE device search.")
+                #     client.stop_notify('15005991-b131-3396-014c-664c9867b917')
+                #     client.stop_notify('6eb675ab-8bd1-1b9a-7444-621e52ec6823')
+                #     await disconnected_event.wait()
+                #     print("Connected: {0}".format(await client.is_connected()))
+                #     await asyncio.sleep(
+                #         0.5
+                #     )
+                # client.set_disconnected_callback(disconnect_callback)
 
-        await client.start_notify('15005991-b131-3396-014c-664c9867b917', notification_handler)
-        await client.start_notify('6eb675ab-8bd1-1b9a-7444-621e52ec6823', notification_handler)
+                services = await client.get_services()
+                for s in services:
+                    for char in s.characteristics:
+                        # print(f'Characteristic: {char}')
+                        print(f'[{char.uuid}] {char.description}:, {char.handle}, {char.properties}')
+                        handle_desc_pairs[char.handle] = (char.description + ':')
 
-        while True:
-            await asyncio.sleep(5.0)
+                await client.start_notify('15005991-b131-3396-014c-664c9867b917', notification_handler)
+                await client.start_notify('6eb675ab-8bd1-1b9a-7444-621e52ec6823', notification_handler)
+
+                while True:
+                    await asyncio.sleep(5.0)
+
+        except BleakDotNetTaskError:
+            print("Peripheral busy. Trying again.")
+            pass
+        except BleakError:
+            print("Didn't connect in time. Retrying.")
+            pass
+
 
 if __name__ == "__main__":
     global handle_desc_pairs
     handle_desc_pairs = {}
+
+    if not path.exists(os.getcwd() + '/data/streamed_data.csv'):
+        os.makedirs(os.getcwd() + '/data', exist_ok=True)
+        new_file_headers = pd.DataFrame(columns=['Time:', 'Temp Value:', 'Strain Value:'])
+        new_file_headers.to_csv('data/streamed_data.csv', encoding='utf-8', index=False)
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    loop.run_until_complete(run(loop))
+
--- parse_data.py
+++ parse_data.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python
 import csv
 import numpy as np # we will use this later, so import it now
-import pandas
+import pandas as pd
 from datetime import date
-from bokeh.io import output_notebook, output_file, show
-from bokeh.plotting import figure
-
-
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.plotting import figure, output_file, show
+from bleak import BleakClient
 
 
 def read_data():
-    with open('data/generated_data.csv', 'r') as f:
-        csv_reader = csv.DictReader(f)
-        line_count = 0
+    # csv file to be read in
+    in_csv = 'data/generated_data.csv'
 
-        datetimes = []
-        values = []
-        iteritems = iter(csv_reader)
-        next(csv_reader)
-        for row in iteritems:
-            datetimes.append(row["Time:"])
-            values.append(row["Reading:"])
+    # csv to write data to
+    out_csv = 'path/to/write/file.csv'
 
-    return datetimes, values
+    # get the number of lines of the csv file to be read
+    number_lines = sum(1 for row in (open(in_csv)))
+
+    # size of chunks of data to write to the csv
+    chunksize = 10
+
+    # start looping through data writing it to a new file for each chunk
+    for i in range(1, number_lines, chunksize):
+        df = pd.read_csv(in_csv,
+                         header=None,
+                         nrows=chunksize,  # number of rows to read at each loop
+                         skiprows=i)  # skip rows that have been read
+
+        df.to_csv(out_csv,
+                  index=False,
+                  header=False,
+                  mode='a',  # append data to csv file
+                  chunksize=chunksize)  # size of data to append for each loop
+
 
 
 def draw_data(x_values, y_values):
-    output_notebook()
-    p = figure(x_axis_type="datetime", title="Flex Readings", plot_width=800, plot_height=800)
+    p = figure()
+    p.line(x_axis_type="datetime", title="Flex Readings", plot_width=800, plot_height=800)
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_alpha = 0.5
-    p.xaxis.axis_label = 'Times (ms)'
-    p.yaxis.axis_label = 'Value (%)'
+    # p.xaxis.axis_label = 'Times (ms)'
+    # p.yaxis.axis_label = 'Value (%)'
 
     p.line(x=x_values, y=y_values, color='blue', line_width=2)
     output_file('test.html')
@@ -40,5 +52,23 @@ def draw_data(x_values, y_values):
 
 
 if __name__ == "__main__":
-    x, y = read_data()
-    draw_data(x, y)
+    data = pd.read_csv('data/generated_data.csv')
+    print(data)
+    source = ColumnDataSource(data)
+    p = figure()
+    p.line(x='Time:', y='Reading:', source=source)
+
+    p.plot_width = 800
+    p.plot_height = 800
+    p.title.text = 'Temperature Readings'
+    p.xaxis.axis_label = 'Time of Reading'
+    p.yaxis.axis_label = 'Readings'
+
+    hover = HoverTool()
+    hover.tooltips = [
+        ('Time of Reading', '@Time:'),
+        ('Reading Value', '@Reading:'),
+    ]
+    p.add_tools(hover)
+
+    show(p)
